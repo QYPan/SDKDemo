2be6019a567aaf6e61bf8ff1f17394f98b28d021
panqingyou <panqingyou@agora.io>
2021-06-04 15:37:11 +0800

    disable some unfinished code

--- SDKdemo/WinEnumerImpl.cpp
+++ SDKdemo/WinEnumerImpl.cpp
@@ -10,10 +10,6 @@
 #include <atlbase.h>
 #include <atlstr.h>
 
-#include "process.h"
-
-using namespace app::utils::process;
-
 namespace app {
 namespace utils {
 
@@ -152,7 +148,7 @@ BOOL WINAPI WindowEnumCallback(HWND hwnd,
 	info.isMinimizeWindow = ::IsIconic(hwnd);
 
 	int thumbWidth = 0, thumbHeight = 0;
-	info.hBitmap = GetProcessIconBitmap(module_name, &thumbWidth, &thumbHeight);
+	//info.hBitmap = GetProcessIconBitmap(module_name, &thumbWidth, &thumbHeight);
     
     auto windows = (std::map<std::string, std::list<WindowEnumer::WINDOW_INFO>>*)data;
     auto itr = windows->find(info.moduleName);
